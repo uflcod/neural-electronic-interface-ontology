f01294cbbd417cda4ac68e13c3c70c3b53036732
Bill Duncan <wdduncan@users.noreply.github.com>
2024-02-20 13:21:58 -0500

    add bionic devices template

---
+++ src/ontology/templates/make-bionic-vision-devices.sh
@@ -0,0 +1,8 @@
+robot template \
+	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
+	  --prefix "dcterms: http://purl.org/dc/terms/" \
+	  --input ../neio-edit.owl \
+	  --template bionic-vision-devices.tsv \
+	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/bionic-vision-devices_import.owl \
+	convert --format ofn \
+	--output ../imports/bionic-vision-devices.owl
--- src/ontology/templates/make-organizations.sh
+++ src/ontology/templates/make-organizations.sh
@@ -1,5 +1,6 @@
 robot template \
 	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
+	  --prefix "dcterms: http://purl.org/dc/terms/" \
 	  --input ../neio-edit.owl \
 	  --template organizations.tsv \
 	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/organizations_import.owl \
