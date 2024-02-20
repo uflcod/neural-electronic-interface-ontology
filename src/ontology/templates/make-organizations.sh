156b45dd195f32583cf7991a5f0cb855de881a82
Bill Duncan <wdduncan@users.noreply.github.com>
2024-02-20 10:34:06 -0500

    add organizations template and import

---
+++ src/ontology/templates/make-organizations.sh
@@ -0,0 +1,7 @@
+robot template \
+	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
+	  --input ../neio-edit.owl \
+	  --template organizations.tsv \
+	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/organizations_import.owl \
+	convert --format ofn \
+	--output ../imports/organizations.owl 
