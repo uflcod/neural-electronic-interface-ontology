0f9ddf9f531c519553ca24471c2d12964f398ed6
Bill Duncan <wdduncan@users.noreply.github.com>
2024-02-20 14:31:41 -0500

    move templates directory

--- src/templates/make-bionic-vision-devices.sh
+++ src/templates/make-bionic-vision-devices.sh
@@ -1,9 +1,9 @@
 robot merge \
-	  --input ../neio-edit.owl \
+	  --input ../ontology/neio-edit.owl \
 	template \
 	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
 	  --prefix "dcterms: http://purl.org/dc/terms/" \
 	  --template bionic-vision-devices.tsv \
 	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/bionic-vision-devices_import.owl \
 	convert --format ofn \
-	--output ../imports/bionic-vision-devices_import.owl
+	--output ../ontology/imports/bionic-vision-devices_import.owl
--- src/templates/make-organizations.sh
+++ src/templates/make-organizations.sh
@@ -1,9 +1,9 @@
 robot merge \
-	  --input ../neio-edit.owl \
+	  --input ../ontology/neio-edit.owl \
 	template \
 	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
 	  --prefix "dcterms: http://purl.org/dc/terms/" \
 	  --template organizations.tsv \
 	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/organizations_import.owl \
 	convert --format ofn \
-	--output ../imports/organizations.owl 
+	--output ../ontology/imports/organizations.owl 
