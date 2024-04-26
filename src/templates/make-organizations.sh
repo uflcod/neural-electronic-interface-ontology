a8a8c37d4fd0365ffd77da9d79092896d1741dc1
Gopikrishnan M. Chandrasekharan <47677575+gopi-kris@users.noreply.github.com>
2024-04-26 17:01:35 -0400

    Origin organizations added
    ID ranges modified. Make shell script modified to correct output filename. neio-edit.OWL imports updated.

--- src/templates/make-organizations.sh
+++ src/templates/make-organizations.sh
@@ -6,4 +6,4 @@ robot merge \
 	  --template organizations.tsv \
 	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/organizations_import.owl \
 	convert --format ofn \
-	--output ../ontology/imports/organizations_import.owl.owl 
+	--output ../ontology/imports/organizations_import.owl 
