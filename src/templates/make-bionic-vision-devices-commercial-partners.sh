291e64a3d1c6fedde924f64df8c815ba0aec35f4
Bill Duncan <wdduncan@users.noreply.github.com>
2024-04-26 17:51:19 -0400

    update: modifying template for commerical parterns and originating organizations

--- src/templates/make-bionic-vision-devices-commercial-partners.sh
+++
@@ -1,9 +0,0 @@
-robot merge \
-	  --input ../ontology/neio-edit.owl \
-	template \
-	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
-	  --prefix "dcterms: http://purl.org/dc/terms/" \
-	  --template bionic-vision-device-commercial-partners.tsv \
-	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/bionic-vision-device-commercial-partners_import.owl \
-	convert --format ofn \
-	--output ../ontology/imports/bionic-vision-device-commercial-partners_import.owl
--- src/templates/make-bionic-vision-devices-origin-organizations.sh
+++
@@ -1,9 +0,0 @@
-robot merge \
-	  --input ../ontology/neio-edit.owl \
-	template \
-	  --prefix "neio: https://w3id.org/neural-electronic-interface-ontology/NEIO_" \
-	  --prefix "dcterms: http://purl.org/dc/terms/" \
-	  --template bionic-vision-device-origin-organizations.tsv \
-	  --ontology-iri https://w3id.org/neural-electronic-interface-ontology/neio/imports/bionic-vision-device-origin-organizations_import.owl \
-	convert --format ofn \
-	--output ../ontology/imports/bionic-vision-device-origin-organizations_import.owl
